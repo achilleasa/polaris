e1509999a9b8bcd55874e7e17a619debea9c350d
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2016-02-23 08:35:58 +0000

    Bind kernel arguments and copy output to host's render target

--- tracer/opencl/cl_tracer.go
+++ tracer/opencl/cl_tracer.go
@@ -41,8 +41,16 @@ type clTracer struct {
 	// A kernel that is run for each screen pixel.
 	traceKernel cl.Kernel
 
-	// Local opencl workgroup size.
-	localWorkgroupSize uint64
+	// Device buffers where the kernel writes its output and frustrum corners.
+	traceOutput     cl.Mem
+	frustrumCorners cl.Mem
+
+	// The scene to be rendered.
+	scene *scene.Scene
+
+	// The output frame dimensions.
+	frameW uint
+	frameH uint
 
 	// A channel for receiving block requests from the renderer.
 	blockReqChan chan tracer.BlockRequest
@@ -93,33 +101,8 @@ func (tr *clTracer) SpeedEstimate() float32 {
 
 // Shutdown and cleanup tracer.
 func (tr *clTracer) Close() {
-	tr.Lock()
-	defer tr.Unlock()
-
-	if tr.ctx == nil {
-		return
-	}
-
-	// Signal worker to exit and wait till it exits
-	close(tr.closeChan)
-	tr.wg.Wait()
-
-	if tr.traceKernel != nil {
-		cl.ReleaseKernel(tr.traceKernel)
-		tr.traceKernel = nil
-	}
-	if tr.traceProgram != nil {
-		cl.ReleaseProgram(tr.traceProgram)
-		tr.traceProgram = nil
-	}
-	if tr.cmdQueue != nil {
-		cl.ReleaseCommandQueue(tr.cmdQueue)
-		tr.cmdQueue = nil
-	}
-	if tr.ctx != nil {
-		cl.ReleaseContext(tr.ctx)
-		tr.ctx = nil
-	}
+	// Lock tracer and perform cleanup
+	tr.cleanup(true)
 }
 
 // Attach tracer to render target and start processing incoming block requests.
@@ -131,23 +114,32 @@ func (tr *clTracer) Setup(sc *scene.Scene, frameW, frameH uint) error {
 		return ErrAlreadyAttached
 	}
 
-	err := tr.setupKernel(sc)
+	err := tr.setupKernel(sc, frameW, frameH)
 	if err != nil {
 		return err
 	}
 
-	readyChan := make(chan struct{}, 0)
+	// Save scene and frame dims
+	tr.scene = sc
+	tr.frameW = frameW
+	tr.frameH = frameH
 
+	readyChan := make(chan struct{}, 0)
 	tr.wg.Add(1)
 	go func() {
 		defer tr.wg.Done()
 		var blockReq tracer.BlockRequest
+		var err error
 		close(readyChan)
 		for {
 			select {
 			case blockReq = <-tr.blockReqChan:
 				// Render block and reply with our completion status
-				tr.process(blockReq)
+				err = tr.process(blockReq)
+				if err != nil {
+					blockReq.ErrChan <- err
+					continue
+				}
 				blockReq.DoneChan <- blockReq.BlockH
 			case <-tr.closeChan:
 				return
@@ -169,14 +161,136 @@ func (tr *clTracer) Enqueue(blockReq tracer.BlockRequest) {
 	}
 }
 
+// Cleanup tracer resources optionally using a lock.
+func (tr *clTracer) cleanup(useLock bool) {
+	if useLock {
+		tr.Lock()
+		defer tr.Unlock()
+	}
+
+	if tr.ctx == nil {
+		return
+	}
+
+	// Signal worker to exit and wait till it exits
+	close(tr.closeChan)
+	tr.wg.Wait()
+
+	if tr.traceOutput != nil {
+		cl.ReleaseMemObject(tr.traceOutput)
+		tr.traceOutput = nil
+	}
+	if tr.frustrumCorners != nil {
+		cl.ReleaseMemObject(tr.frustrumCorners)
+		tr.frustrumCorners = nil
+	}
+	if tr.traceKernel != nil {
+		cl.ReleaseKernel(tr.traceKernel)
+		tr.traceKernel = nil
+	}
+	if tr.traceProgram != nil {
+		cl.ReleaseProgram(tr.traceProgram)
+		tr.traceProgram = nil
+	}
+	if tr.cmdQueue != nil {
+		cl.ReleaseCommandQueue(tr.cmdQueue)
+		tr.cmdQueue = nil
+	}
+	if tr.ctx != nil {
+		cl.ReleaseContext(tr.ctx)
+		tr.ctx = nil
+	}
+}
+
 // Process block request.
-func (tr *clTracer) process(blockReq tracer.BlockRequest) {
-	// @todo
-	fmt.Printf("ToDo: process block request (BlockY %d, %d rows)\n", blockReq.BlockY, blockReq.BlockH)
+func (tr *clTracer) process(blockReq tracer.BlockRequest) error {
+	// Copy camera frustrum corners to allocated buffer.
+	errCode := cl.EnqueueWriteBuffer(
+		tr.cmdQueue,
+		tr.frustrumCorners,
+		cl.TRUE,
+		0,
+		4*16,
+		unsafe.Pointer(&tr.scene.Camera.Frustrum[0]),
+		0,
+		nil,
+		nil,
+	)
+	if errCode != cl.SUCCESS {
+		tr.logger.Printf("Failed to write frustrum corner data")
+		return ErrCopyingDataToDevice
+	}
+
+	// Set kernel params
+	errCode = cl.SetKernelArg(tr.traceKernel, 0, 8, unsafe.Pointer(&tr.traceOutput))
+	if errCode != cl.SUCCESS {
+		tr.logger.Printf("Failed to write kernel arg 0")
+		return ErrSettingKernelArguments
+	}
+	errCode = cl.SetKernelArg(tr.traceKernel, 1, 8, unsafe.Pointer(&tr.frustrumCorners))
+	if errCode != cl.SUCCESS {
+		tr.logger.Printf("Failed to write kernel arg 1")
+		return ErrSettingKernelArguments
+	}
+	errCode = cl.SetKernelArg(tr.traceKernel, 2, 4, unsafe.Pointer(&blockReq.BlockY))
+	if errCode != cl.SUCCESS {
+		tr.logger.Printf("Failed to write kernel arg 2")
+		return ErrSettingKernelArguments
+	}
+	errCode = cl.SetKernelArg(tr.traceKernel, 3, 4, unsafe.Pointer(&blockReq.SamplesPerPixel))
+	if errCode != cl.SUCCESS {
+		tr.logger.Printf("Failed to write kernel arg 3")
+		return ErrSettingKernelArguments
+	}
+	errCode = cl.SetKernelArg(tr.traceKernel, 4, 4, unsafe.Pointer(&blockReq.Exposure))
+	if errCode != cl.SUCCESS {
+		tr.logger.Printf("Failed to write kernel arg 4")
+		return ErrSettingKernelArguments
+	}
+
+	// Execute kernel
+	workOffset := []uint64{0, uint64(blockReq.BlockY)}
+	workSize := []uint64{uint64(tr.frameW), uint64(blockReq.BlockH)}
+	errCode = cl.EnqueueNDRangeKernel(
+		tr.cmdQueue,
+		tr.traceKernel,
+		2,
+		(*uint64)(unsafe.Pointer(&workOffset[0])),
+		(*uint64)(unsafe.Pointer(&workSize[0])),
+		nil,
+		0,
+		nil,
+		nil,
+	)
+	if errCode != cl.SUCCESS {
+		return ErrKernelExecutionFailed
+	}
+
+	// Wait for the kernel to finish executing
+	cl.Finish(tr.cmdQueue)
+
+	// Copy the rendered block from device buffer to the render target
+	readOffset := uint64(tr.frameW * 4 * 4 * blockReq.BlockY)
+	blockSizeBytes := uint64(tr.frameW * 4 * 4 * blockReq.BlockH)
+	errCode = cl.EnqueueReadBuffer(
+		tr.cmdQueue,
+		tr.traceOutput,
+		cl.TRUE,
+		readOffset,     // start at beginning of block
+		blockSizeBytes, // read just the block data
+		unsafe.Pointer(&blockReq.RenderTarget[readOffset]),
+		0,
+		nil,
+		nil,
+	)
+	if errCode != cl.SUCCESS {
+		return ErrCopyingDataToHost
+	}
+	return nil
 }
 
 // Generate and compile the opencl kernel to be used by this tracer.
-func (tr *clTracer) setupKernel(sc *scene.Scene) error {
+func (tr *clTracer) setupKernel(sc *scene.Scene, frameW, frameH uint) error {
 	// Load kernel template
 	templateFile, err := os.Open(tracerSourceFile)
 	if err != nil {
@@ -207,26 +321,31 @@ func (tr *clTracer) setupKernel(sc *scene.Scene) error {
 
 		cl.GetProgramBuildInfo(tr.traceProgram, tr.device.Id, cl.PROGRAM_BUILD_LOG, uint64(len(data)), unsafe.Pointer(&data[0]), &dataLen)
 		tr.logger.Printf("Error building kernel (log follows):\n%s\n", string(data[0:dataLen-1]))
-		cl.ReleaseProgram(tr.traceProgram)
-		tr.traceProgram = nil
+		tr.cleanup(false)
 		return ErrProgramBuildFailed
 	}
 
 	// Fetch kernel entrypoint and query global and local workgroup info
 	tr.traceKernel = cl.CreateKernel(tr.traceProgram, cl.Str("tracePixel"+"\x00"), errPtr)
 	if errPtr != nil && cl.ErrorCode(*errPtr) != cl.SUCCESS {
-		cl.ReleaseProgram(tr.traceProgram)
-		tr.traceProgram = nil
+		tr.cleanup(false)
 		return ErrKernelCreationFailed
 	}
 
-	errCode = cl.GetKernelWorkGroupInfo(tr.traceKernel, tr.device.Id, cl.KERNEL_WORK_GROUP_SIZE, 8, unsafe.Pointer(&tr.localWorkgroupSize), nil)
-	if errCode != cl.SUCCESS {
-		cl.ReleaseKernel(tr.traceKernel)
-		tr.traceKernel = nil
-		cl.ReleaseProgram(tr.traceProgram)
-		tr.traceProgram = nil
-		return ErrGettingWorkgroupInfo
+	// Allocate an output buffer large enough to fit a full frame even
+	// though it will never be fully used if more than one tracers are used.
+	tr.traceOutput = cl.CreateBuffer(*tr.ctx, cl.MEM_WRITE_ONLY, cl.MemFlags(4*4*frameW*frameH), nil, errPtr)
+	if errPtr != nil && cl.ErrorCode(*errPtr) != cl.SUCCESS {
+		tr.cleanup(false)
+		return ErrAllocatingBuffers
 	}
+
+	// Allocate buffer for passing frustrum corners (4 x Vec4 = 64 bytes)
+	tr.frustrumCorners = cl.CreateBuffer(*tr.ctx, cl.MEM_READ_ONLY, 4*4*4, nil, errPtr)
+	if errPtr != nil && cl.ErrorCode(*errPtr) != cl.SUCCESS {
+		tr.cleanup(false)
+		return ErrAllocatingBuffers
+	}
+
 	return nil
 }

93acedd88d17c82cdccc13a3cd0d0ae6fde649e9
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2016-07-27 22:23:25 +0100

    Update debug command to use the new multi-stage pipeline system

--- cmd/debug.go
+++ cmd/debug.go
@@ -3,12 +3,10 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/achilleasa/go-pathtrace/scene"
 	"github.com/achilleasa/go-pathtrace/scene/reader"
 	"github.com/achilleasa/go-pathtrace/tracer"
+	"github.com/achilleasa/go-pathtrace/tracer/opencl"
 	"github.com/achilleasa/go-pathtrace/tracer/opencl/device"
-	"github.com/achilleasa/go-pathtrace/tracer/opencl/integrator"
-	"github.com/achilleasa/go-pathtrace/types"
 	"github.com/urfave/cli"
 )
 
@@ -36,79 +34,70 @@ func findDevice(names []string) (*device.Device, error) {
 func Debug(ctx *cli.Context) error {
 	setupLogging(ctx)
 
-	dev, err := findDevice([]string{"AMD", "Iris", "CPU"})
+	// Load scene
+	sc, err := reader.ReadScene(ctx.Args().First())
+
+	if err != nil {
+		logger.Error(err)
+		return err
+	}
+
+	// Setup tracer
+	dev, err := findDevice([]string{"AMD", "CPU", "Iris", "CPU"})
 	if err != nil {
 		logger.Error(err)
 		return err
 	}
 	logger.Noticef(`using device "%s"`, dev.Name)
 
-	in := integrator.NewMonteCarloIntegrator(dev)
-	err = in.Init()
+	tr, _ := opencl.NewTracer("tr-0", dev)
+	err = tr.Init(
+		frameW,
+		frameH,
+		// Stages
+		opencl.ClearAccumulator(),
+		opencl.PerspectiveCamera(),
+		opencl.MonteCarloIntegrator(3),
+		opencl.TonemapSimpleReinhard(3.5),
+		opencl.DebugFrameBuffer("debug-fb.png"),
+	)
 	if err != nil {
 		logger.Error(err)
 		return err
 	}
-	defer in.Close()
+	defer tr.Close()
 
-	sc, err := reader.ReadScene("../go-pathtrace-scenes/crytek-sponza/sponza.zip")
-	if err != nil {
-		logger.Error(err)
-		return err
+	blockReq := tracer.BlockRequest{
+		FrameW:   frameW,
+		FrameH:   frameH,
+		BlockY:   0,
+		BlockH:   frameH,
+		DoneChan: make(chan uint32, 0),
+		ErrChan:  make(chan error, 0),
 	}
 
-	err = in.UploadSceneData(sc)
-	if err != nil {
-		logger.Error(err)
-		return err
-	}
-
-	blockReq := &tracer.BlockRequest{
-		FrameW: frameW,
-		FrameH: frameH,
-		BlockY: 0,
-		BlockH: frameH,
-	}
-
-	camera := scene.NewCamera(45)
-	camera.Position = types.Vec3{-1053.478, 92.0336, -22.42906}
-	camera.Up = types.Vec3{0, 1, 0}
-	camera.LookAt = types.Vec3{-1, 0, 0}
-
-	camera.SetupProjection(float32(blockReq.FrameW) / float32(blockReq.FrameH))
-	camera.Update()
-
-	err = in.UploadCameraData(camera)
+	// Upload data
+	err = tr.UploadSceneData(sc)
 	if err != nil {
 		logger.Error(err)
 		return err
 	}
 
-	err = in.ResizeOutputFrame(blockReq.FrameW, blockReq.FrameH)
+	sc.Camera.SetupProjection(float32(blockReq.FrameW) / float32(blockReq.FrameH))
+	err = tr.UploadCameraData(sc.Camera)
 	if err != nil {
 		logger.Error(err)
 		return err
 	}
 
-	_, err = in.GeneratePrimaryRays(blockReq)
-	if err != nil {
+	// Run pipeline
+	tr.Enqueue(blockReq)
+	select {
+	case <-blockReq.DoneChan:
+		logger.Notice("Done")
+		return nil
+	case err := <-blockReq.ErrChan:
 		logger.Error(err)
 		return err
 	}
-
-	elapsedTime, err := in.RayIntersectionQuery(blockReq.FrameW * blockReq.FrameH)
-	if err != nil {
-		logger.Error(err)
-		return err
-	}
-
-	logger.Noticef("Elapsed time: %d ms", elapsedTime.Nanoseconds()/1e6)
-
-	err = in.DebugIntersections(blockReq.FrameW, blockReq.FrameH, "debug.png")
-	if err != nil {
-		logger.Error(err)
-		return err
-	}
-
-	return nil
 }
